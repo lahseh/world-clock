c49daf790df25d6de40ffd623c214be9ac611b0f
lahseh <orisaleyeblessing@gmail.com>
2023-11-02 17:36:06 +0000

    added current local time

--- javascript/index.js
+++ javascript/index.js
@@ -28,6 +28,9 @@ setInterval(function () {
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "my-location") {
+    cityTimeZone = moment.tz.guess();
+  }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
